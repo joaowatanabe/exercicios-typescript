1134865a55ce121b59be3ef68807df1f519e2d7b
wabemusic <jvwatanabe@gmail.com>
2024-09-15 16:54:48 -0300

    Exercício 03

--- 02/index.ts
+++ 02/index.ts
@@ -17,3 +17,5 @@ const livros:string[] = [
 ]
 
 console.log(buscarLivro(livros, 'A Divina Comédia'));
+
+// Usando o buscarLivros com o método de arrays indexOf (Percorre o array encontrando a posição e retornando, caso não encontre retorna -1), dentro das condiçoes do buscar livros tipamos o livros sendo um array de strings (:string[]),  e o nomeLivro o livro a ser encontrado (:string), e buscarLivro retorna uma sting (:string).
---
+++ 03/index.ts
@@ -0,0 +1,14 @@
+const validarArray = (numeros: number[]): string => {
+	// verifica se todos são pares
+  const ehpar = numeros.every((elementoAtual) => elementoAtual % 2 === 0);
+
+  return ehpar ? 'Array Válido' : 'Array Inválido'
+};
+
+const numeros: number[] = [0, 122, 4, 6, 7, 8, 44];
+
+console.log(validarArray(numeros));
+
+
+
+
